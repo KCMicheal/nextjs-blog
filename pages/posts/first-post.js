e84b736d654ac324b96e5f2a045100cf5efa0fde
KCMicheal <kcmicheal@users.noreply.github.com>
2021-05-18 16:59:43 +0100

    added update

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Head from 'next/head'
-import Layout from '../components/layout'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
   return (
@@ -9,7 +9,7 @@ export default function FirstPost() {
             <Head>
             <title>First Post</title>
         </Head>
-        <h1>First Post</h1>
+        <h1>First Post by...</h1>
         <h2>
             <Link href="/">
             <a>Back to home</a>
